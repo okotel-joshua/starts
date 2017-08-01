8d95be7a04b13d42878ed92c1024295d5d8159cf
Your Name <okotel18@yahoo.com>
2017-08-01 12:31:56 -0600

    first commit

---
+++ beaver/Controllers/VideosController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using beaver.Models;
+using System.Data.Entity;
+
+namespace beaver.Controllers
+{
+    public class VideosController : Controller
+    {
+
+        private ApplicationDbContext _dbContext;
+        public VideosController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Videos
+        public ActionResult Index()
+        {
+            var videos = _dbContext.Videos.ToList();
+            return View(videos);
+        }
+        public ActionResult New()
+        {
+            return View();
+        }
+        public ActionResult Add(Video video)
+        {
+            _dbContext.Videos.Add(video);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(int id)
+        {
+            var video = _dbContext.Videos.SingleOrDefault(v => v.Id == id);
+
+            if (video == null)
+                return HttpNotFound();
+
+            return View(video);
+        }
+        [HttpPost]
+        public ActionResult Update(Video video)
+        {
+            var videoInDb = _dbContext.Videos.SingleOrDefault(v => v.Id == video.Id);
+
+            if (videoInDb == null)
+                return HttpNotFound();
+
+            videoInDb.Title = video.Title;
+            videoInDb.Description = video.Description;
+            videoInDb.Genre = video.Genre;
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int id)
+        {
+            var video = _dbContext.Videos.SingleOrDefault(v => v.Id == id);
+
+            if (video == null)
+                return HttpNotFound();
+
+            return View(video);
+        }
+        [HttpPost]
+        public ActionResult DoDelete(int id)
+        {
+            var video = _dbContext.Videos.SingleOrDefault(v => v.Id == id);
+            if (video == null)
+                return HttpNotFound();
+            _dbContext.Videos.Remove(video);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ beaver/Models/Video.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace beaver.Models
+{
+    public class Video
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Genre Genre { get; set; }
+
+
+    }
+    public enum Genre
+    {
+        Comedy = 1,
+        horror = 2,
+        Scifi = 3,
+        Romance = 4,
+        documentary = 5,
+        kids = 6,
+    }
+}
